7b641ec6650370b0a40f4459fefff5ef72ed5704
bhaskar rao barla <bhaskar.r.barla@gmail.com>
2017-06-25 07:58:04 +0530

    spring rest template

---
+++ spring-resttemplate/src/main/java/com/restclient/spring/Application.java
@@ -0,0 +1,33 @@
+package com.restclient.spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	//private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			ResponseEntity<String> quote = restTemplate.getForEntity("http://gturnquist-quoters.cfapps.io/api/random", String.class);
+			//log.info(quote.toString());
+			System.out.println("*******************************"+quote);
+		};
+	}
+}
